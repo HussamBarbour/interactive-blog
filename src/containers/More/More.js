7de449834e5677498697df1de1621b79f986db1b
Ebru Oglakkaya <ebruoglakkaya@gmail.com>
2021-02-28 22:48:07 +0300

    Log out functions

--- src/containers/More/More.js
+++ src/containers/More/More.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { SafeAreaView, View,Text } from 'react-native'
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
 import global_styles from '../../styles/global';
 import {ListItem} from '../../components';
 import styles from './styles';
 
-export function More({logout}) {
-    return (
-        <SafeAreaView  style={global_styles.page_container}>
-            <View style={[styles.container,global_styles.top_space]}>
-                <ListItem title="Giriş Yap" goTo="Login"/>
-                <ListItem title="Kayıt Ol"  goTo="Register" />
-                <ListItem title="Çıkış Yap" onPress={logout}/>
-            </View>
-        </SafeAreaView>
-    );
+export function More({logout, user}) {
+  return (
+    <SafeAreaView style={global_styles.page_container}>
+      <View style={[styles.container, global_styles.top_space]}>
+        {!user ? (
+          <View>
+            <ListItem title="Giriş Yap" goTo="Login" />
+            <ListItem title="Kayıt Ol" goTo="Register" />
+          </View>
+        ) : (
+          <ListItem title="Çıkış Yap" onPress={logout} />
+        )}
+      </View>
+    </SafeAreaView>
+  );
 }
--- src/screens/MoreScreen/MoreScreen.js
+++ src/screens/MoreScreen/MoreScreen.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { More } from '../../containers'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useDispatch , useSelector} from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 
 export function MoreScreen({navigation}) {
-    
+    const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
-    function logout(){
+    async function logout(){
+        await AsyncStorage.removeItem('@USER');
         dispatch({ type: 'SET_USER', payload: { user: null } });
     }
 
     return (
         
-        <More isLoged={true} logout={logout}/>
+        <More isLoged={true} logout={logout} user={user}/>
     );
 }
